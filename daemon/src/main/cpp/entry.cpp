4e591d5c32672d1861d37bfa115cac0bacc61aa3
wyq0918dev <wyq0918dev@gmail.com>
2026-07-14 04:02:45 +0800

    commit

--- daemon/src/main/cpp/entry.cpp
+++ daemon/src/main/cpp/entry.cpp
@@ -2,13 +2,13 @@
 #include "entry.hpp"
 #include "jvm_manager.hpp"
 
-JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
+extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
 
     JvmManager::getInstance().init(vm);
 
     return JNI_VERSION_1_6;
 }
 
-JNIEXPORT void JNICALL JNI_OnUnload(JavaVM* vm, void* reserved) {
+extern "C" JNIEXPORT void JNICALL JNI_OnUnload(JavaVM* vm, void* reserved) {
 
 }
